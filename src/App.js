2642b1fb7f5d5880996c813d900fd3cd7ed449ab
IgorHamzi <igorwhamzi@gmail.com>
2021-11-10 12:05:38 -0300

    adiciona requsito 5

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// Requisito 4 -Realizado com a ajuda do Ivan Mastrangelo - turma 16A
+// Requisito 4 - Realizado com a ajuda do Ivan Mastrangelo - turma 16A
 import React from 'react';
 import Form from './components/Form';
 import Card from './components/Card';
@@ -24,7 +24,39 @@ class App extends React.Component {
   onInputChange({ target }) {
     const { name } = target;
     const value = (target.type === 'checkbox') ? target.checked : target.value;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, () => { this.enableDisableBtn(); });
+  }
+
+  enableDisableBtn() {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+    const att1 = parseInt(cardAttr1, 10);
+    const att2 = parseInt(cardAttr2, 10);
+    const att3 = parseInt(cardAttr3, 10);
+    const some = att1 + att2 + att3;
+    const max = 210;
+    const maxAtt = 90;
+    if (
+      cardName === ''
+      || cardDescription === ''
+      || cardImage === ''
+      || cardRare === ''
+      || some > max
+      || att1 > maxAtt || att1 < 0
+      || att2 > maxAtt || att2 < 0
+      || att3 > maxAtt || att3 < 0
+    ) {
+      this.setState({ isSaveButtonDisabled: true });
+    } else {
+      this.setState({ isSaveButtonDisabled: false });
+    }
   }
 
   render() {
